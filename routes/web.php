a27f1b69b7370f870d7f49e227ff75338d7d6f06
Gabriel <2866037@alu.murciaeduca.es>
2025-11-05 09:39:32 +0100

    actualizacion del ejercicio de rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,67 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hola', function()
+{
+    return '¡Hola mundo!';
+});
+
+Route::any('foo/bar', function()
+{
+    return '¡Hola mundo!';
+});
+
+// Funcion de prueba para saludar (con restriccion de solo letras y con el ? hacemos que sea opcional)
+Route::get('saluda/{nombre?}', function($nombre = 'paloma')
+{
+    return '¡Hola ' . $nombre . '!';
+}) ->where('nombre', '[A-Za-z]+');
+
+// Función para sumar dos números (con restriccion de solo números)
+Route::get('/suma/{num1}/{num2}', function($num1, $num2)
+{
+    $suma = $num1 + $num2;
+    return "La suma de $num1 y $num2 es =  $suma";
+})
+->where(array('num1' => '[0-9]+', 'num2' => '[0-9]+'));
+
+
+// Rutas
+Route::get('/', function()
+{
+    return "Pantalla pricnipal";
+});
+
+Route::get('login', function()
+{
+    return "Login usuario";
+});
+Route::get('logout', function()
+{
+    return "Logout usuario";
+});
+
+Route::get('familias-profesionales/show/{id}', function($id)
+{
+    return "Vista detalle proyecto $id";
+})->where('id', '[0-9]+');
+
+Route::get('familias-profesionales/create', function()
+{
+    return "Añadir proyecto";
+})->where('id', '[0-9]+');
+
+Route::get('familias-profesionales/edit/{id}', function($id)
+{
+    return "Modificar proyecto $id";
+})->where('id', '[0-9]+');
+
+Route::get('perfil/{id?}', function($id = null)
+{
+    if($id){
+        return "Visualizar el perfil de $id";
+    } else {
+        return "Visualizar el perfil propio";
+    }
+})->where('id', '[0-9]+');
