10cc30f6dc84d986fae246858a5fd994482ca87e
Antonio Martínez <1627990@alu.murciaeduca.es>
2025-11-24 09:54:30 +0100

    Vistas Index y show

--- resources/views/familias-profesionales/show.blade.php
+++ resources/views/familias-profesionales/show.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.master')
 
 @section('content')
-    Mostrar la familia profesional con ID: {{$id}}
+
+    <div class="row m-4">
+
+        <div class="col-sm-4">
+            {{-- TODO: Imagen de familias profesionales --}}
+            <img src="{{ asset('/images/mp-logo.png') }}" alt="" style="width: 100px" />
+
+
+        </div>
+        <div class="col-sm-8">
+            {{-- TODO: Datos de las familias profesionales --}}
+            <h2>{{ $familiaProfesional['codigo']}}</h2>
+            <h2>{{ $familiaProfesional['nombre']}}</h2>
+        </div>
+    </div>
+
 @stop
