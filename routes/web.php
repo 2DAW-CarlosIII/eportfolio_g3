2dd84ada5ca2a75fd8d5511894dda078c84d1068
Gabriel <2866037@alu.murciaeduca.es>
2025-11-24 09:29:16 +0100

    modificacion web.php gabriel

--- routes/web.php
+++ routes/web.php
@@ -6,66 +6,36 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('hola', function()
-{
-    return '¡Hola mundo!';
+Route::get('login', function() {
+    return view('auth.login');
 });
 
-Route::any('foo/bar', function()
-{
-    return '¡Hola mundo!';
-});
-
-// Funcion de prueba para saludar (con restriccion de solo letras y con el ? hacemos que sea opcional)
-Route::get('saluda/{nombre?}', function($nombre = 'paloma')
-{
-    return '¡Hola ' . $nombre . '!';
-}) ->where('nombre', '[A-Za-z]+');
-
-// Función para sumar dos números (con restriccion de solo números)
-Route::get('/suma/{num1}/{num2}', function($num1, $num2)
-{
-    $suma = $num1 + $num2;
-    return "La suma de $num1 y $num2 es =  $suma";
-})
-->where(array('num1' => '[0-9]+', 'num2' => '[0-9]+'));
-
-
-// Rutas
-Route::get('/', function()
-{
-    return "Pantalla pricnipal";
-});
-
-Route::get('login', function()
-{
-    return "Login usuario";
-});
-Route::get('logout', function()
-{
+Route::get('logout', function() {
     return "Logout usuario";
 });
 
-Route::get('familias-profesionales/show/{id}', function($id)
-{
-    return "Vista detalle proyecto $id";
-})->where('id', '[0-9]+');
-
-Route::get('familias-profesionales/create', function()
-{
-    return "Añadir proyecto";
-})->where('id', '[0-9]+');
-
-Route::get('familias-profesionales/edit/{id}', function($id)
-{
-    return "Modificar proyecto $id";
-})->where('id', '[0-9]+');
-
-Route::get('perfil/{id?}', function($id = null)
-{
+Route::get('perfil/{id?}', function($id = null) {
     if($id){
         return "Visualizar el perfil de $id";
     } else {
         return "Visualizar el perfil propio";
     }
 })->where('id', '[0-9]+');
+
+// ----------------------------------------
+Route::prefix('familias-profesionales')->group(function () {
+   Route::get('/', [FamiliasProfesionalesController::class, 'getIndex']);
+
+
+   Route::get('create', [FamiliasProfesionalesController::class, 'getCreate']);
+
+
+   Route::get('show/{id}', [FamiliasProfesionalesController::class, 'getShow']) -> where('id', '[0-9]+');
+
+
+   Route::get('edit/{id}', [FamiliasProfesionalesController::class, 'getEdit']) -> where('id', '[0-9]+');
+
+   Route::post('store', [FamiliasProfesionalesController::class, 'store']);
+
+   Route::put('update/{id}', [FamiliasProfesionalesController::class, 'update']) -> where('id', '[0-9]+');
+});
