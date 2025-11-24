0b2007c86aebd610994e8f9f1b7adf68bcc57b1a
Antonio Martínez <1627990@alu.murciaeduca.es>
2025-11-24 08:21:12 +0100

    vista index y show

--- resources/views/familias-profesionales/index.blade.php
+++ resources/views/familias-profesionales/index.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
-    Pantalla principal
+    <div class="row">
+
+        @foreach ($familiasProfesionales as $key => $proyecto)
+            <div class="col-4 col-6-medium col-12-small">
+                <section class="box">
+                    <a href="#" class="image featured"><img src="{{ asset('/images/mp-logo.png') }}" alt="" /></a>
+                    <header>
+                        <h3>{{ $familiasProfesionales['nombre'] }}</h3>
+                    </header>
+                    <p>
+                        <a href="http://github.com/2DAW-CarlosIII/{{ $familiasProfesionales['dominio'] }}">
+                            http://github.com/2DAW-CarlosIII/{{ $familiasProfesionales['dominio'] }}
+                        </a>
+                    </p>
+                    <footer>
+                        <ul class="actions">
+                            <li><a href="{{ action([App\Http\Controllers\ProyectosController::class, 'getShow'], ['id' => $key]) }}"
+                                    class="button alt">Más info</a></li>
+                        </ul>
+                    </footer>
+                </section>
+            </div>
+        @endforeach
 @stop
--- resources/views/familias-profesionales/show.blade.php
+++ resources/views/familias-profesionales/show.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.master')
 
 @section('content')
-    Mostrar la familia profesional con ID: {{$id}}
+
+    <div class="row m-4">
+
+        <div class="col-sm-4">
+            {{-- TODO: Imagen de familias profesionales --}}
+            <img src="{{ asset('/images/mp-logo.png') }}" alt="" style="width: 100px" />
+
+
+        </div>
+        <div class="col-sm-8">
+            {{-- TODO: Datos de familias profesionales --}}
+            <h2>{{$familiasProfesionales['docente_id']}}</h2>
+            <h2>{{ $familiasProfesionales['nombre']}}</h2>
+            <h2><a href="http://github.com/2DAW-CarlosIII/{{ $familiasProfesionales['dominio'] }}">
+                http://github.com/2DAW-CarlosIII/{{ $familiasProfesionales['dominio'] }}</a></h2>
+
+            @foreach ($familiasProfesionales['metadatos'] as $key => $metadato)
+                <li>{{$key}} : {{$metadato}}</li>
+            @endforeach
+
+            @if ($familiasProfesionales['metadatos']['calificacion']>=5)
+                <p>familia profesional aprobada</p>
+                <button type="button" class="btn btn-success">Suspender familia profesional</button>
+            @else
+                <p>familia profesional suspensa</p>
+                <button type="button" class="btn btn-primary" style="background-color: blue">Aprobar familia profesional </button>
+            @endif
+        </div>
+    </div>
+
 @stop
