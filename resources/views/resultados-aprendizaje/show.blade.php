217341a315223fd73f7e5e6a62da296be56dfa66
Gabriel <2866037@alu.murciaeduca.es>
2025-12-03 10:00:24 +0100

    ejercicio eportfolio

---
+++ resources/views/resultados-aprendizaje/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="row m-4">
+
+        <div class="col-sm-4">
+            {{-- TODO: Imagen de familias profesionales --}}
+            <img src="{{ asset('/images/mp-logo.png') }}" alt="" style="width: 100px" />
+
+
+        </div>
+        <div class="col-sm-8">
+            {{-- TODO: Datos de las familias profesionales --}}
+            <h2>{{ $familiasProfesionales['codigo'] }}</h2>
+            <h2>{{ $familiasProfesionales['nombre'] }}</h2>
+            <ul class="actions">
+                <li><a href="{{ action([App\Http\Controllers\FamiliasProfesionalesController::class, 'getShow'], ['id' => $id]) }}"
+                        class="button alt">Editar proyecto</a>
+                </li>
+                <li><a href="{{ action([App\Http\Controllers\FamiliasProfesionalesController::class, 'getIndex']) }}"
+                        class="button alt">Volver al listado</a>
+                </li>
+            </ul><br>
+        </div>
+    </div>
+
+@stop
