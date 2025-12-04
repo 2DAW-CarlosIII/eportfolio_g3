12983234502bd0a0c761d4c3c770641ab9514eb3
Gabriel <2866037@alu.murciaeduca.es>
2025-12-04 11:27:46 +0100

    routes arreglado

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FamiliasProfesionalesController;
+use App\Http\Controllers\ResultadosAprendizajesController;
 
 Route::get('/', function () {
     return view('home');
@@ -40,4 +41,21 @@ Route::prefix('familias-profesionales')->group(function () {
    Route::post('store', [FamiliasProfesionalesController::class, 'store']);
 
    Route::put('update/{id}', [FamiliasProfesionalesController::class, 'update']) -> where('id', '[0-9]+');
+});
+
+Route::prefix('resultados-aprendizaje')->group(function () {
+   Route::get('/', [ResultadosAprendizajesController::class, 'getIndex']);
+
+
+   Route::get('create', [ResultadosAprendizajesController::class, 'getCreate']);
+
+
+   Route::get('show/{id}', [ResultadosAprendizajesController::class, 'getShow']) -> where('id', '[0-9]+');
+
+
+   Route::get('edit/{id}', [ResultadosAprendizajesController::class, 'getEdit']) -> where('id', '[0-9]+');
+
+   Route::post('store', [ResultadosAprendizajesController::class, 'store']);
+
+   Route::put('update/{id}', [ResultadosAprendizajesController::class, 'update']) -> where('id', '[0-9]+');
 });
