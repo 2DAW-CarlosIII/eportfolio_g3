b209c3634693de148d13967a996c23de4aeee5ba
Paul <2907767@alu.murciaeduca.es>
2025-12-11 12:08:08 +0100

    Menú Log In

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\ResultadosAprendizajesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
