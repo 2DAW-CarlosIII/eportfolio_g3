a25e47945089d395644d254666b67516e10600b3
Paul G. Collado <2907767@alu.murciaeduca.es>
2025-12-11 08:42:14 +0100

    Espacios quitados
    Entre las '->' no habrán espacios

--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,11 @@ Route::get('perfil/{id?}', function($id = null) {
 Route::prefix('familias-profesionales')->group(function () {
    Route::get('/', [FamiliasProfesionalesController::class, 'getIndex']);
    Route::get('create', [FamiliasProfesionalesController::class, 'getCreate']);
-   Route::get('show/{id}', [FamiliasProfesionalesController::class, 'getShow']) -> where('id', '[0-9]+');
-   Route::get('edit/{id}', [FamiliasProfesionalesController::class, 'getEdit']) -> where('id', '[0-9]+');
+   Route::get('show/{id}', [FamiliasProfesionalesController::class, 'getShow'])->where('id', '[0-9]+');
+   Route::get('edit/{id}', [FamiliasProfesionalesController::class, 'getEdit'])->where('id', '[0-9]+');
 
    Route::post('store', [FamiliasProfesionalesController::class, 'store']);
-   Route::put('update/{id}', [FamiliasProfesionalesController::class, 'update']) -> where('id', '[0-9]+');
+   Route::put('update/{id}', [FamiliasProfesionalesController::class, 'update'])->where('id', '[0-9]+');
 });
 
 
@@ -44,10 +44,10 @@ Route::prefix('familias-profesionales')->group(function () {
 Route::prefix('criterios-evaluacion')->group(function () {
    Route::get('/', [CriteriosEvaluacionController::class, 'getIndex']);
    Route::get('create', [CriteriosEvaluacionController::class, 'getCreate']);
-   Route::get('show/{id}', [CriteriosEvaluacionController::class, 'getShow']) -> where('id', '[0-9]+');
-   Route::get('edit/{id}', [CriteriosEvaluacionController::class, 'getEdit']) -> where('id', '[0-9]+');
+   Route::get('show/{id}', [CriteriosEvaluacionController::class, 'getShow'])->where('id', '[0-9]+');
+   Route::get('edit/{id}', [CriteriosEvaluacionController::class, 'getEdit'])->where('id', '[0-9]+');
    Route::post('store', [CriteriosEvaluacionController::class, 'postCreate']);
-   Route::put('update/{id}', [CriteriosEvaluacionController::class, 'putCreate']) -> where('id', '[0-9]+');
+   Route::put('update/{id}', [CriteriosEvaluacionController::class, 'putCreate'])->where('id', '[0-9]+');
 });
 
 
@@ -55,11 +55,11 @@ Route::prefix('criterios-evaluacion')->group(function () {
 Route::prefix('ciclos-formativos')->group(function () {
    Route::get('/', [CiclosFormativosController::class, 'getIndex']);
    Route::get('create', [CiclosFormativosController::class, 'getCreate']);
-   Route::get('show/{id}', [CiclosFormativosController::class, 'getShow']) -> where('id', '[0-9]+');
-   Route::get('edit/{id}', [CiclosFormativosController::class, 'getEdit']) -> where('id', '[0-9]+');
+   Route::get('show/{id}', [CiclosFormativosController::class, 'getShow'])->where('id', '[0-9]+');
+   Route::get('edit/{id}', [CiclosFormativosController::class, 'getEdit'])->where('id', '[0-9]+');
 
    Route::post('store', [CiclosFormativosController::class, 'store']);
-   Route::put('update/{id}', [CiclosFormativosController::class, 'update']) -> where('id', '[0-9]+');
+   Route::put('update/{id}', [CiclosFormativosController::class, 'update'])->where('id', '[0-9]+');
 });
 
 
@@ -67,9 +67,9 @@ Route::prefix('ciclos-formativos')->group(function () {
 Route::prefix('resultados-aprendizaje')->group(function () {
    Route::get('/', [ResultadosAprendizajesController::class, 'getIndex']);
    Route::get('create', [ResultadosAprendizajesController::class, 'getCreate']);
-   Route::get('show/{id}', [ResultadosAprendizajesController::class, 'getShow']) -> where('id', '[0-9]+');
-   Route::get('edit/{id}', [ResultadosAprendizajesController::class, 'getEdit']) -> where('id', '[0-9]+');
+   Route::get('show/{id}', [ResultadosAprendizajesController::class, 'getShow'])->where('id', '[0-9]+');
+   Route::get('edit/{id}', [ResultadosAprendizajesController::class, 'getEdit'])->where('id', '[0-9]+');
 
    Route::post('store', [ResultadosAprendizajesController::class, 'store']);
-   Route::put('update/{id}', [ResultadosAprendizajesController::class, 'update']) -> where('id', '[0-9]+');
+   Route::put('update/{id}', [ResultadosAprendizajesController::class, 'update'])->where('id', '[0-9]+');
 });
