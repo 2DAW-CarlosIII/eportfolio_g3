d7a572d47cf8190edf27907e3871b445df6659f2
Adrian_C <3341802@alu.murciaeduca.es>
2026-01-20 09:55:31 +0100

    Controladores y rutas api, recursos de criterios de evaluacion, resultados de aprendizaje y matriculas

--- app/Models/Matricula.php
+++ app/Models/Matricula.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Matricula extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'estudiante_id',
+        'modulo_formativo_id'
+    ];
 }
